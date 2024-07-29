73a7377121af53e89805eed915791811473bc4d5
fwcd <30873659+fwcd@users.noreply.github.com>
2024-07-29 17:23:00 +0200

    Add `scanner.c` to bindings

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -2,6 +2,7 @@ package tree_sitter_kotlin
 
 // #cgo CFLAGS: -std=c11 -fPIC
 // #include "../../src/parser.c"
+// #include "../../src/scanner.c"
 // // NOTE: if your language has an external scanner, add it here.
 import "C"
 
