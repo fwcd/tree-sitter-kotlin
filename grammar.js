3e713475b474911839ce8e11ac70d892719a2eed
Jesper Andersen <jespera@gmail.com>
2026-04-07 22:38:40 +0200

    Allow trailing comma in function-block params
    I.e. allow
    
    { x, y, ->
      ...
    }

--- grammar.js
+++ grammar.js
@@ -657,6 +657,7 @@ module.exports = grammar({
     function_type_parameters: $ => prec.left(1, seq(
       "(",
       optional(sep1(choice($.parameter, $._type), ",")),
+      optional(","),
       ")"
     )),
 
@@ -963,7 +964,7 @@ module.exports = grammar({
       ),
     ),
 
-    lambda_parameters: $ => sep1($._lambda_parameter, ","),
+    lambda_parameters: $ => seq(sep1($._lambda_parameter, ","), optional(",")),
 
     _lambda_parameter: $ => choice(
       $.variable_declaration,
